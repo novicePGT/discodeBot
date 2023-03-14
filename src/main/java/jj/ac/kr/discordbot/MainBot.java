0fd331583738408790ecc3d5ac959b5c973c64a7
novicePGT <dymnam@naver.com>
2023-03-15 00:18:16 +0900

    [feat] : 이모션을 달면 누가 달았는지 기본 채팅채널에 공지하는 기능 추가

--- src/main/java/jj/ac/kr/discordbot/MainBot.java
+++ src/main/java/jj/ac/kr/discordbot/MainBot.java
@@ -1,6 +1,7 @@
 package jj.ac.kr.discordbot;
 
 import io.github.cdimascio.dotenv.Dotenv;
+import jj.ac.kr.discordbot.listener.EventListener;
 import net.dv8tion.jda.api.OnlineStatus;
 import net.dv8tion.jda.api.entities.Activity;
 import net.dv8tion.jda.api.sharding.DefaultShardManagerBuilder;
@@ -13,6 +14,11 @@ public class MainBot {
     private final Dotenv config;
     private final ShardManager shardManager;
 
+    /**
+     * 환경 변수를 로드하고, 샤드 매니저를 빌드한다.
+     * @throws LoginException -> 로그인 토큰이 유효하지 않으면 LoginException이 발생한다.
+     */
+
     public MainBot() throws LoginException {
         config = Dotenv.configure().load();
         String token = config.get("TOKEN"); // 토큰 값은 공개되지 않아야함 -> env 파일의 TOKEN을 환경변수로 저장해 가져옴
@@ -22,12 +28,23 @@ public class MainBot {
         builder.setStatus(OnlineStatus.ONLINE); // 아이콘의 상태, 온라인이면 초록색 알갱이를 나타냄.
         builder.setActivity(Activity.watching("자바와 함께")); // 활동하는 것을 나타낸다.
         shardManager = builder.build(); // 토큰이 올바르지 않거나 로그인 예외를 발생시키는지 확인하는 절차를 거침.
+
+        // 이벤트리스너 등록 과정
+        shardManager.addEventListener(new EventListener());
     }
 
+    /**
+     * 샤드 매니저를 리턴한다.
+     * @return
+     */
     public ShardManager getShardManager() {
         return shardManager;
     }
 
+    /**
+     * 봇의 환경 변수를 리턴한다.
+     * @return
+     */
     public Dotenv getConfig() {
         return config;
     }
--- src/main/java/jj/ac/kr/discordbot/listener/EventListener.java
+++ src/main/java/jj/ac/kr/discordbot/listener/EventListener.java
@@ -1,2 +1,22 @@
-package jj.ac.kr.discordbot.listener;public class EventListener {
+package jj.ac.kr.discordbot.listener;
+
+import net.dv8tion.jda.api.events.message.react.MessageReactionAddEvent;
+import net.dv8tion.jda.api.hooks.ListenerAdapter;
+import org.jetbrains.annotations.NotNull;
+
+public class EventListener extends ListenerAdapter {
+
+    StringConversion stringConversion = new StringConversion();
+
+    @Override
+    public void onMessageReactionAdd(@NotNull MessageReactionAddEvent event) {
+        String user = String.valueOf(event.getUser());
+        String reaction = event.getReaction().getReactionEmote().getAsReactionCode();
+        String channelMention = event.getChannel().getAsMention();
+
+        String onlyUserId = stringConversion.splitUserName(user);
+
+        String message = onlyUserId + " 님이  " + channelMention + " 에서  " + reaction + " 으로 반응했습니다 !!";
+        event.getGuild().getDefaultChannel().sendMessage(message).queue();
+    }
 }
