c3d81f87ee3bc014de7abd1c18fcb642540342f7
novicePGT <dymnam@naver.com>
2023-04-03 22:45:48 +0900

    [feat]: 데이터 찾기 테스트 & 데이터베이스 연결 v2

--- src/main/java/jj/ac/kr/discordbot/connection/mariadbcon/DbData.java
+++ src/main/java/jj/ac/kr/discordbot/connection/mariadbcon/DbData.java
@@ -1,2 +1,45 @@
-package jj.ac.kr.discordbot.connection.mariadbcon;public class DbData {
+package jj.ac.kr.discordbot.connection.mariadbcon;
+
+import java.sql.*;
+
+public class DbData {
+
+    private final String JDBC_DRIVER = "org.mariadb.jdbc.Driver";
+    private final String DB_URL = "jdbc:mariadb://localhost:3306/discordBot_db";
+    private final String USER = "root";
+    private final String PASS = "1208";
+
+    private String sql = "SELECT * FROM url_table";
+
+    private Connection connection = null;
+    private Statement statement = null;
+
+    public String findToDb(String data) throws SQLException {
+        String valueUrl = null;
+        try {
+            Class.forName(JDBC_DRIVER);
+            connection = DriverManager.getConnection(DB_URL, USER, PASS);
+            statement = connection.createStatement();
+            ResultSet resultSet = statement.executeQuery(sql);
+
+            while (resultSet.next()) {
+                String valueColum = resultSet.getString("urlName");
+                if (valueColum.equals(data)) {
+                    valueUrl = resultSet.getString("url");
+                }
+            }
+            System.out.println("[DB]: 정보 조회 완료 ");
+        } catch (ClassNotFoundException e) {
+            throw new SQLException();
+        } finally {
+            if (statement != null) {
+                statement.close();
+            }
+            if (connection != null) {
+                connection.close();
+            }
+        }
+
+        return valueUrl;
+    }
 }
