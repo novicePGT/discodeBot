79b0e1d4dfee414f85a80ce724065c625c8a5486
novicePGT <dymnam@naver.com>
2023-04-20 13:22:43 +0900

    [refactor]: 문제 추가

--- src/main/java/jj/ac/kr/discordbot/commands/question/QuestionSet.java
+++ src/main/java/jj/ac/kr/discordbot/commands/question/QuestionSet.java
@@ -10,7 +10,8 @@ public class QuestionSet {
     List<ExamItem> examItems = new ArrayList<>();
 
     private String[] examQuestion = {
-            "자바의 장점", "자바의 단점", "자바 컴파일러", "자바 바이트코드", "자바 가상머신(JVM)"
+            "자바의 장점", "자바의 단점", "자바 컴파일러", "자바 바이트코드", "자바 가상머신(JVM)", "가상머신의 구성 요소", "Java 8", "리터럴(literal)", "산술 연산자",
+            "대입 연산자", "증감 연산자"
     };
 
     public List<ExamItem> questionInform() {
