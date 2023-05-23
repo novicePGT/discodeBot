b4daf96cd7b3d75152dfed7468543915ed043db1
novicePGT <dymnam@naver.com>
2023-05-23 13:39:09 +0900

    [feat]: skip 기능추가

---
+++ src/main/java/jj/ac/kr/discordbot/music/Skip.java
@@ -0,0 +1,54 @@
+package jj.ac.kr.discordbot.music;
+
+import jj.ac.kr.discordbot.commands.ICommand;
+import net.dv8tion.jda.api.entities.GuildVoiceState;
+import net.dv8tion.jda.api.entities.Member;
+import net.dv8tion.jda.api.events.interaction.command.SlashCommandInteractionEvent;
+import net.dv8tion.jda.api.interactions.commands.build.OptionData;
+
+import java.util.List;
+
+public class Skip implements ICommand {
+    @Override
+    public String getName() {
+        return "skip";
+    }
+
+    @Override
+    public String getDescription() {
+        return "Will skip the current song";
+    }
+
+    @Override
+    public List<OptionData> getOptions() {
+        return null;
+    }
+
+    @Override
+    public void execute(SlashCommandInteractionEvent event) {
+        Member member = event.getMember();
+        GuildVoiceState voiceState = member.getVoiceState();
+
+        if (!voiceState.inAudioChannel()) {
+            event.reply("음악을 스킵하기 전에 방에 먼저 들어가주세요.").setEphemeral(true).queue();
+            return;
+        }
+
+        Member self = event.getGuild().getSelfMember();
+        GuildVoiceState selfVoiceState = self.getVoiceState();
+
+        if (!selfVoiceState.inAudioChannel()) {
+            event.reply("같은 채널 안에 있어야합니다.").setEphemeral(true).queue();
+            return;
+        }
+
+        if (selfVoiceState.getChannel() != voiceState.getChannel()) {
+            event.reply("같은 채널 안에 있지 않습니다").setEphemeral(true).queue();
+            return;
+        }
+
+        GuildMusicManager guildMusicManager = PlayerManager.get().getGuildMusicManager(event.getGuild());
+        guildMusicManager.getTrackScheduler().getPlayer().stopTrack();
+        event.reply("Skipped...").queue();
+    }
+}
